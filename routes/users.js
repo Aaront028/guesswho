7ac11966a808ecf907a65643b08250f000698af7
Eli Bing <107372513+elizabeth-bing@users.noreply.github.com>
2022-08-19 10:31:53 +1200

    sort out home route, right and wrong routes

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const db = require('../db')
 
 const router = express.Router()
 
+//GET home page
 router.get('/', (req, res) => {
   db.getUsers()
     .then((users) => {
@@ -14,6 +15,35 @@ router.get('/', (req, res) => {
     })
 })
 
-// insert logic here to render either right or wrong.
+//GET Right page
+router.get('/right/:id', (req, res) => {
+  const id = req.params.id
+  db.getUserById(id)
+    .then((user) => {
+      res.render('right', { user })
+    })
 
+    .catch((err) => {
+      console.error(err)
+      res.status(500).send('Server error')
+    })
+})
+
+//GET wrong page
+router.get('/wrong/:id', (req, res) => {
+  const id = req.params.id
+  db.getUserById(id)
+    .then((user) => {
+      res.render('wrong', { user })
+    })
+
+    .catch((err) => {
+      console.error(err)
+      res.status(500).send('Server error')
+    })
+})
+
+// insert logic here to render either right or wrong.
+//when click try again or return button, need routes for redirect to home
+//USE HBS for redirect to home
 module.exports = router
