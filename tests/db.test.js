902369fbc2719a47b9d7bf071ea9b1c2ae4e29b2
JP Guiang <107418008+jp-guiang@users.noreply.github.com>
2022-08-19 12:29:33 +1200

    added testing

--- tests/db.test.js
+++ tests/db.test.js
@@ -1,35 +1,61 @@
-/* eslint-disable jest/no-conditional-expect */
-const testEnv = require('./test-environment')
-const db = require('../db')
+const knex = require('knex')
+const config = require('../knexfile')
+const test = knex(config.test)
 
-let testDb = null
+const func = require('../db')
+
+beforeAll(() => {
+  return test.migrate.latest()
+})
 
 beforeEach(() => {
-  testDb = testEnv.getTestDb()
-  return testEnv.initialise(testDb)
+  return test.seed.run()
 })
 
-afterEach(() => testEnv.cleanup(testDb))
+afterAll(() => {
+  return func.close(test)
+})
 
-test('getUsers gets all users', () => {
-  // One for each letter of the alphabet!
-  const expected = 3
-  return db
-    .getUsers(testDb)
-    .then((users) => {
-      const actual = users.length
-      expect(actual).toBe(expected)
+describe('read function', () => {
+  it('returns all students in manaia', () => {
+    return func.getUsers(test).then((data) => {
+      expect(data).toHaveLength(5)
     })
-    .catch((err) => expect(err).toBeNull())
+  })
 })
 
-test('getUser gets a single user', () => {
-  const expected = 'test user 1'
-  return db
-    .getUser(99901, testDb)
-    .then((user) => {
-      const actual = user.name
-      expect(actual).toBe(expected)
-    })
-    .catch((err) => expect(err).toBeNull())
-})
+// /* eslint-disable jest/no-conditional-expect */
+// const testEnv = require('./test-environment')
+// const db = require('../db')
+
+// let testDb = null
+
+// beforeEach(() => {
+//   testDb = testEnv.getTestDb()
+//   return testEnv.initialise(testDb)
+// })
+
+// afterEach(() => testEnv.cleanup(testDb))
+
+// test('getUsers gets all users', () => {
+//   // One for each letter of the alphabet!
+//   const expected = 3
+//   return db
+//     .getUsers(testDb)
+//     .then((users) => {
+//       const actual = users.length
+//       expect(actual).toBe(expected)
+//     })
+//     .catch((err) => expect(err).toBeNull())
+// })
+
+// test('getUser gets a single user', () => {
+//   const expected = 'test user 1'
+//   return db
+//     .getUserById(99901, testDb)
+//     .then((user) => {
+//       const actual = user.name
+//       expect(actual).toBe(expected)
+//     })
+//     .catch((err) => expect(err).toBeNull())
+// })
